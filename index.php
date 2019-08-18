e008248229c727d3d43b5404cc7abfb5521c1e97
paulwiki <bkodossoubabapaul@gmail.com>
2019-08-18 16:52:46 +0000

    [IMPR] Add remark in the code PHP

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 	 * Auteurs: Jean Olivier Yao & Paul Bouaffou ;
 
-	 * Description: Programme permettant de donner le nombre de contributions d'un utilisateur
+	 * Description: Programme donnant le nombre de contributions d'un utilisateur
 	 				sur les Projets WIKI en langue française (Exemples: Wikipédia, Wikimedia Commons,
 	 				Wikiquote, Wikidata, Wikitionnaire, ...). [NB]: Il s'exécute en ligne de commande ; 
 
@@ -16,11 +16,13 @@
 	echo "Veuillez renseigner votre nom d'utilisateur \n";
 	$username = readline();
 
-	// Tableau numéroté de lien des projets WIKI
+	// Tableau numéroté 1 de lien des projets WIKI
 	$project_wiki_name = array('fr.wikipedia.org', 'commons.wikimedia.org', 'fr.wikiquote.org', 'wikidata.org', 'fr.wiktionary.org');
 
-	// Tableau numéroté de nom des projets WIKI
+	// Tableau numéroté 2 de nom des projets WIKI
 	$project_wiki_link = array('Wikipedia', 'Wikimedia Commons', 'Wikiquote', 'Wikidata', 'Wikitionnaire');
+
+	// Découpage de l'URL
 
 	$link_1 = 'https://';
 
@@ -28,10 +30,12 @@
 
 	$link_3 = '&usprop=editcount';
 
+	// Variable qui regroupera tous les éléments de l'URL ci-dessus
 	$link_all = NULL;
 	
-	// Boucle faisant l'alternance des éléments du tableau numéroté afin de traiter chaque lien wiki.
-	for ($link_wiki=0; $link_wiki < 5; $link_wiki++) {	
+	// Boucle faisant l'alternance des éléments du tableau numéroté 1 afin de traiter chaque lien wiki.
+	for ($link_wiki=0; $link_wiki < 5; $link_wiki++) {
+
 		// Formation de l'URL
 		$link_all = $link1.$project_wiki_link[$link_wiki].$link2.urlencode($username).$link3;
 
@@ -41,13 +45,15 @@
 		// convert json string to array
 		$contributions = json_decode($link_content, true);
 
-	
+		// Vérification de l'existence des paramètres "query" & "users"
 		if (isset($contributions["query"]["users"])) {
 
+			// Boucle faisant l'alternance des éléments du tableau numéroté 2 afin d'afficher chaque nom de projet wiki.
 			for ($name_wiki=0; $name_wiki < 5; $name_wiki++) { 
 				
 				foreach ($contributions["query"]["users"] as $content) {
 
+					// Affichage du nombre de contributions de l'utilisateur
 					echo "\n Vous avez" . $content["editcount"] ." contribution(s) en  " . project_wiki_name[$name_wiki] . "\n";
 
 					echo "\n";
