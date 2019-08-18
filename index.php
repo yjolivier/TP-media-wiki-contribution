ab0c492099321f2c8443d678842b2f736740c952
paulwiki <bkodossoubabapaul@gmail.com>
2019-08-18 16:35:23 +0000

    [IMPR] Editing of file

--- index.php
+++ index.php
@@ -1,24 +1,64 @@
 <?php
-	//Saisie du username
-	echo "Veuillez renseigner le nom d'utilisateur \n";
+
+	/*
+
+	 * Auteurs: Jean Olivier Yao & Paul Bouaffou ;
+
+	 * Description: Programme permettant de donner le nombre de contributions d'un utilisateur
+	 				sur les Projets WIKI en langue française (Exemples: Wikipédia, Wikimedia Commons,
+	 				Wikiquote, Wikidata, Wikitionnaire, ...). [NB]: Il s'exécute en ligne de commande ; 
+
+	 * Licence: MIT ;
+
+	*/
+
+	// Saisie du username
+	echo "Veuillez renseigner votre nom d'utilisateur \n";
 	$username = readline();
-	$link = 'https://fr.wikipedia.org/w/api.php?list=usercontribs&ucuser='.urlencode($username).'&format=json&action=query&uclimit=10';
 
-	//Recuperation du contenu du lien pour le mettre dans une variable $link_content 
-	$link_content = file_get_contents($link); 
+	// Tableau numéroté de lien des projets WIKI
+	$project_wiki_name = array('fr.wikipedia.org', 'commons.wikimedia.org', 'fr.wikiquote.org', 'wikidata.org', 'fr.wiktionary.org');
 
-	//convert json string to array
-	$contributions = json_decode($link_content, true);
+	// Tableau numéroté de nom des projets WIKI
+	$project_wiki_link = array('Wikipedia', 'Wikimedia Commons', 'Wikiquote', 'Wikidata', 'Wikitionnaire');
+
+	$link_1 = 'https://';
+
+	$link_2 = '/w/api.php?action=query&format=json&list=users&ususers=';
+
+	$link_3 = '&usprop=editcount';
+
+	$link_all = NULL;
+	
+	// Boucle faisant l'alternance des éléments du tableau numéroté afin de traiter chaque lien wiki.
+	for ($link_wiki=0; $link_wiki < 5; $link_wiki++) {	
+		// Formation de l'URL
+		$link_all = $link1.$project_wiki_link[$link_wiki].$link2.urlencode($username).$link3;
+
+		// Recuperation du contenu du lien pour le mettre dans une variable $link_content 
+		$link_content = file_get_contents($link_all); 
+
+		// convert json string to array
+		$contributions = json_decode($link_content, true);
 
 	
-	if (isset($contributions["query"]["usercontribs"])) {
-		foreach ($contributions["query"]["usercontribs"] as $content) {
-			echo "\n" . $content["user"] ."\n";
-			echo $content["title"] ."\n";
-			echo $content["timestamp"] ."\n";
-			echo $content["comment"] ."\n";
-			echo "\n";
-		}	
+		if (isset($contributions["query"]["users"])) {
+
+			for ($name_wiki=0; $name_wiki < 5; $name_wiki++) { 
+				
+				foreach ($contributions["query"]["users"] as $content) {
+
+					echo "\n Vous avez" . $content["editcount"] ." contribution(s) en  " . project_wiki_name[$name_wiki] . "\n";
+
+					echo "\n";
+				}
+
+			}
+
+		}
+
 	}
+
 	/*var_dump(json_decode($link_content, true)) ;*/
+
 ?>
